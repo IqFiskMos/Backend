644d357b106923f6c33c9a772a6b902e88659b75
Cenny Arvesen <cenny84.caw@gmail.com>
2021-08-15 23:24:53 +0200

    sddf
    sdfsdf

---
+++ Lexicon - MVC/Guessing-Game/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Guessing_Game.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guessing_Game.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult GameView()
+        {
+            int randomNum = GuessingGameModel.GetGuessNumber();
+            HttpContext.Session.SetInt32("rndValue", randomNum);
+            return View();
+        }
+        public IActionResult CheckGuessing(int input)
+        {     
+            int gameNum = (int) HttpContext.Session.GetInt32("rndValue");
+            ViewBag.Message = GuessingGameModel.CheckGuess(input, gameNum);
+            return View("GameView");
+
+        }
+        public IActionResult Reset()
+        {
+            GuessingGameModel.StoreNums.Clear();
+            int randomNum = GuessingGameModel.GetGuessNumber();
+            HttpContext.Session.SetInt32("rndValue", randomNum);
+            return View("GameView");
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lexicon - MVC/Guessing-Game/Models/GuessingGameModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guessing_Game.Models
+{
+    
+    public class GuessingGameModel
+    {
+        public static List<int> StoreNums = new List<int>();   
+        
+        public static int GetGuessNumber()
+        {
+            var random = new Random();
+            int num = random.Next(1, 100);
+            StoreNums.Clear();
+            return num;
+        }
+        public static string CheckGuess(int userInput, int guess)
+        {            
+            if (userInput <= 100 && userInput >= 1)
+            {
+                StoreNums.Add(userInput);
+                if (userInput > guess)
+                {
+                   return "Your guess is too high!";
+                }
+                else if (userInput < guess)
+                {
+                    return "Your guess is too low!";
+                }
+                else
+                {
+                    return "Congratulations! Your guess is correct.";
+                }                
+            }
+            else
+            {
+                return "Please enter a valid number!";
+            }            
+        }
+    }
+}
