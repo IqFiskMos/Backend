5f1be0e5585e0be7a32802002cafd19122e39965
Cenny Arvesen <cenny84.caw@gmail.com>
2021-08-15 23:21:17 +0200

    asdsd
    asdasd

---
+++ Lexicon - MVC/Inputs-and-forms/Controllers/FeverCheckController.cs
@@ -0,0 +1,25 @@
+﻿using FormsAndInputsAssignment.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsAndInputsAssignment.Controllers
+{
+    public class FeverCheckController : Controller
+    {
+        public IActionResult FeverCheckView()
+        {
+            
+            return View();
+        }
+        public ActionResult FeverCheck(double userInput, string type)
+        {
+            ViewBag.result = FeverCheckModel.FeverCheckEvaluation(userInput, type);
+            return View("FeverCheckView");
+        }
+        
+    }
+}
---
+++ Lexicon - MVC/Inputs-and-forms/Models/FeverCheckModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsAndInputsAssignment.Models
+{
+    public class FeverCheckModel
+    {
+        public static string FeverCheckEvaluation(double tempurature, string selctedTempType)
+        {
+            string healthReport = "";
+
+            if (selctedTempType == "celsius")
+            {
+                if (tempurature > 37.5 && tempurature <=40) healthReport = "You have fever. Please take some rest and medicine.";
+                else if (tempurature < 35) healthReport = "You have Hypothermia.";
+                else if (tempurature > 40) healthReport = "You have Hyperpyrexia. Please consult a doctor.";
+                else healthReport = "Nothing to worry. Your temperature is normal.";
+            }
+            
+            if (selctedTempType == "fahrenheit")
+            {
+                if (tempurature > 99.5 && tempurature <= 104) healthReport = "You have fever. Please take some rest and medicine.";
+                else if (tempurature < 95) healthReport = "You have Hypothermia.";
+                else if (tempurature > 104) healthReport = "You have Hyperpyrexia. Please consult a doctor.";
+                else healthReport = "Nothing to worry. Your temperature is normal.";
+            }
+
+            return healthReport;
+        }
+    }
+}
